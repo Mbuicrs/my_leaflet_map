20ca35f124c82aae526e8c2dcff97d897b680a1f
mbui.cyrus <87632264+Mbuicrs@users.noreply.github.com>
2025-06-01 17:03:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Load and display GeoJSON data
+fetch('subsidence.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        layer.bindPopup(`<b>Location:</b> ${feature.properties.name}`);
+      }
+    }).addTo(map);
+  })
+  .catch(error => console.error('Error loading GeoJSON:', error));
+
 // Initialize bounds
 const bounds = new L.LatLngBounds();
 
